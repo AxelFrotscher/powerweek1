97b3abce375804407fe2225ae8e9efa155af2a3b
Axel Frotscher <afrotscher@ikp.tu-darmstadt.de>
2019-06-17 18:04:41 +0200

    Created CMake file for gcc part

---
+++ example/gcc/build/CMakeFiles/banana.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/banana.dir/main.cpp.o"
+  "CMakeFiles/banana.dir/hello_fn.cpp.o"
+  "banana.pdb"
+  "banana"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/banana.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
